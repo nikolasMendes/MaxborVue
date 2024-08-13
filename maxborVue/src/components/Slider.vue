e13225b6107127e6f3ce97a98eebaaccb3dbf867
nikolasMendes <nikolasmendes98@hotmail.com>
2024-08-13 13:59:44 -0300

    correcao url

--- maxborVue/src/components/Slider.vue
+++ maxborVue/src/components/Slider.vue
@@ -5,7 +5,7 @@
       :key="item"
       class="relative h-[480px] w-[850px] rounded-xl mb-10"
     >
-      <img class="h-full rounded-xl" :src="getImage(item.url)" />
+      <img class="h-full rounded-xl" :src="item.url" />
       <div class="absolute top-72 left-10">
         <p class="text-white text-3xl mb-3">{{ item.title }}</p>
         <button
@@ -18,36 +18,36 @@
   </div>
 </template>
 <script>
-import "";
+import "../../static/isolantesEletricos.png";
 export default {
   name: "Slider",
   data() {
     return {
-      imagem: "@/assets/images/espumas-adesivas.png",
+      img: "../../static/isolantesEletricos.png",
       images: [
         {
           title: "Espuma Adesiva",
-          url: "../assets/images/espumas-adesivas.png",
+          url: "../../static/espumasAdesivas.png",
           link: "",
         },
         {
           title: "Isolante Elétrico",
-          url: "../assets/images/isolantesEletricos.png",
+          url: "../../static/isolantesEletricos.png",
           link: "",
         },
         {
           title: "Lençol de Borracha",
-          url: "../assets/images/lencolBorracha.png",
+          url: "../../static/lencolBorracha.png",
           link: "",
         },
         {
           title: "Piso de Borracha",
-          url: "../assets/images/pisosBorracha.png",
+          url: "../../static/pisosBorracha.png",
           link: "",
         },
         {
           title: "Peça Técnica",
-          url: "../assets/images/pecasTecnicas.png",
+          url: "../../static/pecasTecnicas.png",
           link: "",
         },
       ],
