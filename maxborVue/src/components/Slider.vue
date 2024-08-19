a1c6cd3db84420cc31486a9e77b156fa39fed348
nikolasMendes <nikolasmendes98@hotmail.com>
2024-08-19 13:47:02 -0300

    prime vue e vue-carousel

--- maxborVue/src/components/Slider.vue
+++ maxborVue/src/components/Slider.vue
@@ -1,24 +1,41 @@
 <template>
   <div>
-    <div
-      v-for="item in this.images"
-      :key="item"
-      class="relative h-[480px] w-[850px] rounded-xl mb-10"
+    <!-- <div
+        v-for="(item, index) in images"
+        :key="item"
+        class="relative h-[480px] w-[850px] rounded-xl mb-10"
+      >
+        <img class="h-full rounded-xl" :src="item.url" />
+        <div class="absolute top-72 left-10">
+          <p class="text-white text-3xl mb-3">
+            {{ item.title }}
+          </p>
+          <button
+            class="transition rounded-lg ease-in-out delay-150 px-4 py-2 bg-red-600 hover:-translate-y-1 hover:scale-110 hover:bg-red-700 duration-300"
+          >
+            <p class="text-white">Saiba mais</p>
+          </button>
+        </div>
+      </div> -->
+    <Carousel
+      :value="images"
+      :numVisible="1"
+      :numScroll="1"
+      :responsiveOptions="responsiveOptions"
+      circular
+      :autoplayInterval="3000"
     >
-      <img class="h-full rounded-xl" :src="item.url" />
-      <div class="absolute top-72 left-10">
-        <p class="text-white text-3xl mb-3">{{ item.title }}</p>
-        <button
-          class="transition ease-in-out delay-150 px-4 py-2 bg-red-700 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300"
-        >
-          <p class="text-white">Saiba mais</p>
-        </button>
-      </div>
-    </div>
+      <template #item="item">
+        <div>
+          <img :src="item.data.url" />
+        </div>
+      </template>
+    </Carousel>
   </div>
 </template>
 <script>
-import "../../static/isolantesEletricos.png";
+import Carousel from "primevue/carousel";
+
 export default {
   name: "Slider",
   data() {
@@ -51,25 +68,54 @@ export default {
           link: "",
         },
       ],
+      responsiveOptions: [
+        {
+          breakpoint: "1400px",
+          numVisible: 1,
+          numScroll: 1,
+        },
+        {
+          breakpoint: "1199px",
+          numVisible: 1,
+          numScroll: 1,
+        },
+        {
+          breakpoint: "767px",
+          numVisible: 1,
+          numScroll: 1,
+        },
+        {
+          breakpoint: "575px",
+          numVisible: 1,
+          numScroll: 1,
+        },
+      ],
 
       timer: null,
       currentIndex: 0,
     };
   },
+  components: {
+    Carousel,
+  },
   computed: {
-    // currentImg() {
-    //   return this.images[Math.abs(this.currentIndex) % this.images.length].src;
-    // },
+    currentImg() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length].url;
+    },
+    currentTitle() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length]
+        .title;
+    },
   },
   mounted() {
-    //this.startSlide();
+    this.startSlide();
   },
   methods: {
-    startSlide: function () {
-      this.timer = setInterval(this.next, 4000);
+    startSlide() {
+      this.timer = setInterval(this.next(), 4000);
     },
     next() {
-      this.currentIndex += 1;
+      this.index += 1;
     },
     prev() {
       this.currentIndex -= 1;
@@ -80,4 +126,21 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+/* .fade-enter-active,
+.fade-leave-active {
+  transition: all 0.9s ease;
+  overflow: hidden;
+  visibility: visible;
+  position: absolute;
+  width: 100%;
+  opacity: 1;
+}
+
+.fade-enter,
+.fade-leave-to {
+  visibility: hidden;
+  width: 100%;
+  opacity: 0;
+} */
+</style>
--- maxborVue/src/main.js
+++ maxborVue/src/main.js
@@ -3,9 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 
 import "primeicons/primeicons.css";
-
+import VueCarousel from "vue-carousel";
 import router from "./router";
 
 const app = createApp(App);
 app.use(router);
+app.use(VueCarousel);
 app.mount("#app");
